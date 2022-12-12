55b4994eb143423965a65e3347b41332663648a3
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-12-12 18:05:46 +0330

    improve normalize schema

--- src/schema_utils.rs
+++ src/schema_utils.rs
@@ -2,27 +2,58 @@ use regex::Regex;
 
 #[cfg(test)]
 pub fn normalize_schema(sdl: &str) -> String {
-    let re = Regex::new(r"\s+").unwrap();
-    re.replace_all(sdl, " ").trim().to_string()
+    let sdl = Regex::new(r"\s+")
+        .unwrap()
+        .replace_all(sdl, " ")
+        .trim()
+        .to_string();
+    let sdl = Regex::new(r"\s*\{\s*")
+        .unwrap()
+        .replace_all(&sdl, " {\n")
+        .to_string();
+    let sdl = Regex::new(r"\s+(\S+\s*:\s*\S+)")
+        .unwrap()
+        .replace_all(&sdl, "\n  $1")
+        .to_string();
+    let sdl = Regex::new(r"\s*}\s*")
+        .unwrap()
+        .replace_all(&sdl, "\n}\n")
+        .to_string();
+
+    sdl
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
     fn test_normalize_schema() {
         let sdl = "
+
+
             type Query {
                 hello: String!
+                nice: String!
+                bye: String!
             }
-            schema {
-                query: Query
-            }
+
+            schema { query: Query }
         ";
         assert_eq!(
             normalize_schema(sdl),
-            "type Query { hello: String! } schema { query: Query }"
+            indoc! {"
+                type Query {
+                  hello: String!
+                  nice: String!
+                  bye: String!
+                }
+                schema {
+                  query: Query
+                }
+
+"}
         );
     }
 }
